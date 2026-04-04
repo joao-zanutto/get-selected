26474a7c1e920b72f037e1357ed151780acbcd6f
joao-zanutto <jpberno@gmail.com>
2026-04-04 07:18:33 -0300

    run dist

---
+++ dist/625.index.js
@@ -0,0 +1,15 @@
+export const id = 625;
+export const ids = [625];
+export const modules = {
+
+/***/ 625:
+/***/ ((module) => {
+
+module.exports = eval("require")("./main");
+
+
+/***/ })
+
+};
+
+//# sourceMappingURL=625.index.js.map
